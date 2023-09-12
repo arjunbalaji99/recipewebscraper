1d43301b4db6238d4f23b8711128d0521db5daac
arjunbalaji99 <91145107+arjunbalaji99@users.noreply.github.com>
2023-09-12 20:40:53 +0000

    comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 from collections import Counter
 from bs4 import BeautifulSoup
 
+#given an allrecipes page - returns all recipe urls on the page
 def get_recipe_urls(page_url):
     response = requests.get(page_url)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -18,6 +19,7 @@ def get_recipe_urls(page_url):
     
     return recipe_urls
 
+#cleans up ingredient data to just the ingredient (removes all numbers, measurements, etc) - return cleaned ingredient
 def cleans_data(ingredient):
     ingredient = ingredient.text.strip()
     ingredient = re.sub('[0123456789]', "", ingredient) 
@@ -40,6 +42,7 @@ def cleans_data(ingredient):
     
     return ingredient
 
+#given a recipe page url - returns all cleaned ingredients from that recipe
 def scrape_recipe_data(recipe_url):
     response = requests.get(recipe_url)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -53,11 +56,13 @@ def scrape_recipe_data(recipe_url):
 
     return title, ingredients
 
+#calculates and prints the 10 most common ingredients from a list of ingredients
 def calculate_most_common(all_ingredients):
     most_common_ingredients = Counter(all_ingredients).most_common(10)
     for ingredient in most_common_ingredients:
         print(str(ingredient[0]).capitalize() + " - " + str(ingredient[1]) + " occurrences")
 
+#handles all interaction with the user
 def user_interaction():
     print("Hello! Welcome to the Recipe Scraping Ingredient Compiler!")
     page_url = input("If you have a specific recipes page you would like to scrape, input it here! If you do not, that is fine and we can just use one of our pre-selected pages - type NONE in that scenario!")
@@ -73,7 +78,7 @@ def user_interaction():
     print("Scraping " + str(len(recipe_urls)) + " recipes...")
     eta = 0.33 * len(recipe_urls)
     time.sleep(1)
-    print("ETA: " + str('%.2f' % eta) + " seconds")
+    print("ETA: " + str('%.2f' % (eta)) + " seconds")
     for recipe_url in recipe_urls:
         title, ingredients = scrape_recipe_data(recipe_url)
         titles.append(title)
